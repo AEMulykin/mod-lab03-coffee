28058f690c8cdf2be6fb3970ec6cafc3e180b5c6
AEMulykin <124257311+AEMulykin@users.noreply.github.com>
2024-04-14 15:32:44 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,7 @@
 // Copyright 2022 UNN-IASR
+#include <locale.h>
 #include "Automata.h"
 
 int main() {
-    Automata beverageAutomaton;
-    beverageAutomaton.powerOn();
-    beverageAutomaton.insertCoin(800);
-    beverageAutomaton.selectOption(4);
-
-    if (beverageAutomaton.isPurchasePossible()) {
-        beverageAutomaton.brewBeverage();
-        beverageAutomaton.concludeTransaction();
-    } else {
-        beverageAutomaton.reclaimCash();
-    }
-
-    beverageAutomaton.powerOff();
-    return 0;
+return 0;
 }
