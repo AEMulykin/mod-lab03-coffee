d2795da1e2365ce01e624f0a81d976836b5107fb
AEMulykin <124257311+AEMulykin@users.noreply.github.com>
2024-04-14 14:44:01 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,17 @@
 // Copyright 2022 UNN-IASR
-#include <iostream>
-#include <string>
 #include "Automata.h"
 
 int main() {
     Automata beverageAutomaton;
     beverageAutomaton.powerOn();
     beverageAutomaton.insertCoin(800);
-    try {
-        beverageAutomaton.selectOption(4);
-        if (beverageAutomaton.isPurchasePossible()) {
-            beverageAutomaton.brewBeverage();
-            beverageAutomaton.concludeTransaction();
-        } else {
-            beverageAutomaton.reclaimCash();
-        }
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
+    beverageAutomaton.selectOption(4);
+
+    if (beverageAutomaton.isPurchasePossible()) {
+        beverageAutomaton.brewBeverage();
+        beverageAutomaton.concludeTransaction();
+    } else {
+        beverageAutomaton.reclaimCash();
     }
 
     beverageAutomaton.powerOff();
