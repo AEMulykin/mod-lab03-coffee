2c591a3851ab204d8901e3afa70d28e6f88ee395
AEMulykin <124257311+AEMulykin@users.noreply.github.com>
2024-04-14 14:39:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
 // Copyright 2022 UNN-IASR
-#include <iostream>
-#include <string>
 #include "Automata.h"
 
 int main() {
-    Automata drinking_machine;
-    drinking_machine.on();
-    drinking_machine.coin(800);
-    drinking_machine.choice(4);
-    if (drinking_machine.check()) {
-        drinking_machine.cook();
-        drinking_machine.finish();
-    } else {
-        drinking_machine.cancel();
+    Automata beverageAutomaton;  // Create an Automata instance
+    beverageAutomaton.powerOn();  // Power on the beverage machine
+    beverageAutomaton.insertCoin(800);  // Insert money into the machine
+
+    try {
+        beverageAutomaton.selectOption(4);  // Make a selection for the beverage
+        if (beverageAutomaton.isPurchasePossible()) {
+            beverageAutomaton.brewBeverage();  // Start brewing the beverage
+            beverageAutomaton.concludeTransaction();  // Finish transaction once done
+        } else {
+            beverageAutomaton.reclaimCash();  // Reclaim the cash if not enough
+        }
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;  // Handle any exceptions that may arise
     }
-    drinking_machine.off();
-    return 0;
+
+    beverageAutomaton.powerOff();  // Turn the machine off when done
+    return 0;  // Exit the program
 }
